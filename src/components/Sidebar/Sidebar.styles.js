aea40b3df8a49582b9c2c31adb3ff5e7396bd9a8
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-08-18 19:59:11 +0100

    modified underline in sidebar

--- src/components/Sidebar/Sidebar.styles.js
+++ src/components/Sidebar/Sidebar.styles.js
@@ -1,65 +1,70 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 
-export const SidebarContainer = styled.div
-    `
-    width: ${p => p.isSidebarOpen ? '20%' : '5%'};
-    max-width:280px;
-    min-width: 80px;
-    color: #fff;
-    background-image: 
-    linear-gradient(
-        315deg,
-        rgba(17, 7, 0, 0.9) 0%,   
-        rgba(5, 108, 126, 0.5) 74%
-        ),
-        url(${props => props.bgImage});
-    
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
-    position: relative; //toggler Container
-    transition: .4s ease-in all;
-    `
+export const SidebarContainer = styled.div`
+  width: ${(p) => (p.isSidebarOpen ? "20%" : "5%")};
+  max-width: 280px;
+  min-width: 80px;
+  color: #fff;
+  background-image: linear-gradient(
+      315deg,
+      rgba(17, 7, 0, 0.9) 0%,
+      rgba(5, 108, 126, 0.5) 74%
+    ),
+    url(${(props) => props.bgImage});
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  position: relative; //toggler Container
+  transition: 0.4s ease-in all;
+`;
 // header styled component
 export const SidebarHeader = styled.h3`
-        padding: 20px 0;
-        text-align: center;
-        margin-bottom: 10px;
-        letter-spacing: 6px;
-        font-family: ${p => p.font}
-`
+  padding: 20px 0;
+  text-align: center;
+  margin-bottom: 10px;
+  letter-spacing: 6px;
+  font-family: ${(p) => p.font};
+`;
 
-// menu items styled component   
-export const MenuItemContainer = styled.div``; 
-export const ItemContainer = styled.div``; 
+// menu items styled component
+export const MenuItemContainer = styled.div``;
+export const ItemContainer = styled.div``;
 
 export const MenuItem = styled.div`
-        ${p => !p.isSidebarOpen && `
+  ${(p) =>
+    !p.isSidebarOpen &&
+    `
                 text-align: center;
-                ${p.selected && 'background-color: rgba(0, 0, 0, 0.3)'}
+                ${p.selected && "background-color: rgba(0, 0, 0, 0.3)"}
         `};
-        padding: 6px 20px;
-        margin-top: 20px;
-        font-weight: 600;
-        color: ${p => p.selected ? '#fff' : '#beae9a'};
-        font-family: ${p => p.font};
-        white-space: nowrap;
-        position: relative; // Dropdown icon parent
-        transition: .3s ease-in all;
+  padding: 6px 20px;
+  margin-top: 20px;
+  font-weight: 600;
+  color: ${(p) => (p.selected ? "#fff" : "#beae9a")};
+  font-family: ${(p) => p.font};
+  white-space: nowrap;
+  position: relative; // Dropdown icon parent
+  transition: 0.3s ease-in all;
 
-        &:hover {
-                color: #fff;
-                transition: .1s ease-in all;
-        }
+  &:hover {
+    color: #fff;
+    transition: 0.1s ease-in all;
+  }
 
-        &:after {
-                content: '';
-                border: 0.5px solid ${p => p.selected ? '#fff' : '#beae9a'};
-                display: ${p => p.isSidebarOpen && p.selected && p.isOpen ? 'none' : 'block' };
-                margin: 8px 0 4px;
-        }
+  &:after {
+    content: "";
+    border: 0.5px solid ${(p) => (p.selected ? "#fff" : "#beae9a")};
+    display: ${(p) =>
+      p.isSidebarOpen && p.selected && p.isOpen ? "none" : "block"};
+    margin: 8px 0 4px;
+    margin-left: 10%;
+    width: 20%;
+  }
 
-        ${p => !p.selected && `
+  ${(p) =>
+    !p.selected &&
+    `
           &:hover {
                 &:after {
                   border: 0.5px solid rgba(255, 255, 255,0.4);
@@ -67,48 +72,45 @@ export const MenuItem = styled.div`
                 }
           }
         `}
-        
-`; 
+`;
 
-// menu text styled component 
+// menu text styled component
 export const Text = styled.p`
-        display: ${p => p.isSidebarOpen ? 'inline' : 'none'}
-`
-// menu icon styled component 
+  display: ${(p) => (p.isSidebarOpen ? "inline" : "none")};
+`;
+// menu icon styled component
 export const Icon = styled.img`
-        ${p => p.isSidebarOpen && 
-                `padding-right: 20px;
-                transition: .2s ease-in padding-right`
-        };
-        height: 16px;
-        width: 16px;
-       
-`
+  ${(p) =>
+    p.isSidebarOpen &&
+    `padding-right: 20px;
+                transition: .2s ease-in padding-right`};
+  height: 16px;
+  width: 16px;
+`;
 // Submenu items styled component
 export const SubMenuItemContainer = styled.div`
-  font-size: ${p => p.isSidebarOpen ? ' 14px' : '12px'};
-  ${p => p.isSidebarOpen && 'padding-left: 15%'};
-  ${p => !p.isSidebarOpen && 'text-align: center'};
-  
+  font-size: ${(p) => (p.isSidebarOpen ? " 14px" : "12px")};
+  ${(p) => p.isSidebarOpen && "padding-left: 15%"};
+  ${(p) => !p.isSidebarOpen && "text-align: center"};
 `;
 export const SubMenuItem = styled.div`
   color: #beae9a;
 
   &:hover {
-        color: #fff;
+    color: #fff;
   }
 `;
 // Dropdown icon styled component
-export const DropdownIcon = styled.span `
+export const DropdownIcon = styled.span`
   position: absolute;
-  top: ${p => p.isOpen ? '16px' : '12px'};
+  top: ${(p) => (p.isOpen ? "16px" : "12px")};
   right: 24px;
-  border: solid ${p => p.selected ? '#fff' : '#beae9a'};
+  border: solid ${(p) => (p.selected ? "#fff" : "#beae9a")};
   border-width: 0 2px 2px 0;
   padding: 3px;
-  transform: ${p => p.isOpen ? 'rotate(-135deg)' : 'rotate(45deg)'};
-  transition: .4s;
-`
+  transform: ${(p) => (p.isOpen ? "rotate(-135deg)" : "rotate(45deg)")};
+  transition: 0.4s;
+`;
 // Toggler styled components
 export const TogglerContainer = styled.div`
   position: absolute;
@@ -117,22 +119,20 @@ export const TogglerContainer = styled.div`
   left: 0;
   right: 0;
   margin: 0 auto;
-`
+`;
 export const Toggler = styled.div`
- height: 20px;
- cursor: pointer;
- position: relative;
+  height: 20px;
+  cursor: pointer;
+  position: relative;
 
- &:after {
-   content: '';
-   position: absolute;
-   left: 0;
-   top: .25em;
-   height: .1em;
-   width: 100%;
-   background: #fff;
-   box-shadow: 
-    0 .75em 0 0 #fff,
-    0 1.5em 0 0 #fff
- }
-`
+  &:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0.25em;
+    height: 0.1em;
+    width: 100%;
+    background: #fff;
+    box-shadow: 0 0.75em 0 0 #fff, 0 1.5em 0 0 #fff;
+  }
+`;
